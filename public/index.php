42973fb45394c05e470421e72b7634936bf026af
Pronesti <dieh.diego@gmail.com>
2020-01-15 13:03:46 -0300

    new routes

--- public/index.php
+++ public/index.php
@@ -4,18 +4,18 @@ session_start();
 
 $which = $_GET['page'];
 $router = new Library\Router();
-$router->addRouteOld("carrito", new Library\CheckLogin(new Carrito\Paginas\verCarrito()));
-$router->addRouteOld("listado", new Library\CheckLogin(new Carrito\Paginas\verListadoProductos()));
-$router->addRouteOld("register", new Carrito\Paginas\verRegistro());
-$router->addRouteOld("login", new Carrito\Paginas\verLogin());
+$router->addRoute("carrito", new Library\CheckLogin(new Carrito\Paginas\verCarrito()));
+$router->addRoute("listado", new Library\CheckLogin(new Carrito\Paginas\verListadoProductos()));
+$router->addRoute("register", new Carrito\Paginas\verRegistro());
+$router->addRoute("login", new Carrito\Paginas\verLogin());
 
 if(isset($which)) {
-    $page = $router->matchOld($which);
+    $page = $router->match($which);
 } else {
-    $page = $router->matchOld("listado");
+    $page = $router->match("listado");
 }
 if(is_null($page)){
-    $page = $router->matchOld("listado");
+    $page = $router->match("listado");
 }
 if($_SERVER['REQUEST_METHOD'] == 'GET'){
     echo $page->get($_GET, $_POST, $_SESSION);
