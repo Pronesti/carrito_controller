22270b8713e044e4c785859136a41240d3968c69
Diego Pronesti <dieh.diego@gmail.com>
2020-01-16 19:20:17 -0300

    Revert "first commit"
    This reverts commit 27a61ba90fdcba6dd4bc257051cd0937d2544671.

--- src/Paginas/verCarrito.php
+++ src/Paginas/verCarrito.php
@@ -7,7 +7,39 @@ class verCarrito implements \Library\Controller
 {
     public function get($get,$post,&$session)
     {
-        
+        include_once("productos.php");
+        if (!isset($session['carrito'])) {
+            $session['carrito'] = array();
+        }
+        $total = 0;
+        $listaCarrito = "";
+        foreach ($session['carrito'] as $posicion => $itemKey) {
+            foreach ($productos as $key => $item) {
+                if ($key == $itemKey) {
+                    $prodCarrito = new \Library\TemplateEngine("../src/templates/elementoCarrito.template");
+                    $prodCarrito->addVariable("name", $item['name']);
+                    $prodCarrito->addVariable("key", $key);
+                    $prodCarrito->addVariable("posicion", $posicion);
+                    $prodCarrito->addVariable("price", $item['price']);
+                    $prodCarrito->addVariable("url", $item['url']);
+                    $listaCarrito .= $prodCarrito->render();
+                    $total += $item['price'];
+                }
+            }
+        }
+        $te = new \Library\TemplateEngine("../src/templates/index.template");
+        $nav = new \Library\TemplateEngine("../src/templates/navbar.template");
+        $nav->addVariable("username", $_SESSION['username']);
+        $te->addVariable("navbar", $nav->render());
+        $carrito = new \Library\TemplateEngine("../src/templates/carrito.template");
+        $carrito->addVariable("listaCarrito", $listaCarrito);
+        $carrito->addVariable("total", $total);
+        $te->addVariable("contenido",$carrito->render());
+        if (count($session['carrito']) > 0) {
+            return $te->render();
+        } else {
+            return $te->render();
+        }
     }
     public function post($get,$post,&$session){
 
